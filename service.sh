3c1ce1aa1824af832cea99f63c509b33820e8bf3
pspsp <cgorkem704@outlook.com>
2025-12-24 21:29:41 +0300

    service.sh: license: remove lyntaxxq.

--- service.sh
+++ service.sh
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2025 lyntaxxq, lyntaxqq
-# The module maintainer is lyntaxxq
+# Copyright (C) 2025 lyntaxqq
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
